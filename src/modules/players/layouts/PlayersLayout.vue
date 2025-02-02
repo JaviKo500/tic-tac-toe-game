407b581c4ce013164e9a89f7b5f19a217ea164ed
JaViKo500 <javikogutierrez64@gmail.com>
2025-02-02 11:43:06 -0500

    form add player

--- src/modules/players/layouts/PlayersLayout.vue
+++ src/modules/players/layouts/PlayersLayout.vue
@@ -1,9 +1,33 @@
 
 <template>
   <CustomModal :is-open="isOpenCustomModal" >
+    <template #content>
+      <div class="w-full">
+        <div class="my-1">
+          Nickname
+        </div>
+        <input type="text" class="input w-full" placeholder="Type here" />
+
+        <div class="mt-1">
+          Gender
+        </div>
+        <div class="flex gap-2 mt-1">
+          <input type="radio" name="male" class="radio radio-primary"  />
+          Male
+        </div>
+        <div class="flex gap-2 mt-1">
+          <input type="radio" name="female" class="radio radio-primary" />
+          Female
+        </div>
+        <div class="flex gap-2 mt-1">
+          <input type="radio" name="other" class="radio radio-primary" />
+          Other
+        </div>
+      </div>
+    </template>
     <template #footer>
       <button class="btn btn-soft btn-secondary" @click="isOpenCustomModal = false">Close</button>
-      <button class="btn btn-primary">Save</button>
+      <button class="btn btn-primary">Add</button>
     </template>
   </CustomModal>
   <div class="flex justify-center items-center h-screen">
