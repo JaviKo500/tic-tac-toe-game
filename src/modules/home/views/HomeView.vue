52ce0b5e7f33df3b4df28878105fc5927e82dfcd
JaViKo500 <javikogutierrez64@gmail.com>
2025-02-02 10:04:06 -0500

    add width card

--- src/modules/home/views/HomeView.vue
+++ src/modules/home/views/HomeView.vue
@@ -3,14 +3,16 @@
   <HomeNavbar/>
   <div class="flex flex-col items-center justify-center w-full py-10">
     <div className="flex flex-col gap-4 items-center w-80">
-      <CustomCard>
-        <template #content>
-          <div class="flex flex-col gap-4 text-center">
-            <p class="text-1xl font-bold">Total games played</p>
-            <p class="text-xl font-semibold">0</p>
-          </div>
-        </template>
-      </CustomCard>
+      <div class="w-full">
+        <CustomCard>
+          <template #content>
+            <div class="flex flex-col gap-4 text-center">
+              <p class="text-1xl font-bold">Total games played</p>
+              <p class="text-xl font-semibold">0</p>
+            </div>
+          </template>
+        </CustomCard>
+      </div>
       <RouterLink class="btn w-full py-10 btn-soft btn-primary" :to="{ name: 'players-layout' }">
         New Game
       </RouterLink>
