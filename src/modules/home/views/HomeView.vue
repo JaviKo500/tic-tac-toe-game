da52f1888bdd627191f8bf41a8c267944baedfd6
JaViKo500 <javikogutierrez64@gmail.com>
2025-02-01 20:47:46 -0500

    custom card

--- src/modules/home/views/HomeView.vue
+++ src/modules/home/views/HomeView.vue
@@ -3,13 +3,21 @@
   <HomeNavbar/>
   <div class="flex flex-col items-center justify-center w-full py-10">
     <div className="flex flex-col gap-4 items-center w-80">
-      
+      <CustomCard>
+        <template #content>
+          <div class="flex flex-col gap-4 text-center">
+            <p class="text-1xl font-bold">Total games played</p>
+            <p class="text-xl font-semibold">0</p>
+          </div>
+        </template>
+      </CustomCard>
       <button class="btn w-full py-10 btn-soft btn-primary">New Game</button>
       <button class="btn w-full py-8 btn-soft">Options</button>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-  import HomeNavbar from '../components/HomeNavbar.vue';
+  import CustomCard from '@/modules/common/components/CustomCard.vue';
+import HomeNavbar from '../components/HomeNavbar.vue';
   
 </script>
