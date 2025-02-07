dadf041e5329b99f06b4b5c7f1cc99a791092670
JaViKo500 <javikogutierrez64@gmail.com>
2025-02-06 22:27:24 -0500

    update footer

--- src/modules/common/components/CustomFooter.vue
+++ src/modules/common/components/CustomFooter.vue
@@ -1,27 +1,15 @@
 <template>
-  <footer class="footer sm:footer-horizontal bg-base-200 text-base-content p-10 flex-none">
+  <footer class="footer sm:footer-horizontal bg-base-200 text-base-content p-6 flex-none">
     <aside>
     <AzuIcon/>
-    <p>
-      AzuCompany
-      <br />
-      Providing reliable tech since 2021
-    </p>
   </aside>
   <nav>
-    <h6 class="footer-title">Services</h6>
-    <a class="link link-hover">Design</a>
-    <a class="link link-hover">Custom software</a>
-  </nav>
-  <nav>
     <h6 class="footer-title">Company</h6>
-    <a class="link link-hover">About us</a>
-    <a class="link link-hover">Contact</a>
+    <a class="link link-hover">AzuCompany</a>
   </nav>
   <nav>
     <h6 class="footer-title">Legal</h6>
     <a class="link link-hover">Terms of use</a>
-    <a class="link link-hover">Privacy policy</a>
   </nav>
   </footer>
 </template>
