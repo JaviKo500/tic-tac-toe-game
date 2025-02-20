d5d91f9495aad49e5e4d0ccfcbfd33ebe9af466a
JaViKo500 <javikogutierrez64@gmail.com>
2025-02-19 23:06:22 -0500

    fix(ui): update design home

--- src/modules/home/components/HomeNavbar.vue
+++ src/modules/home/components/HomeNavbar.vue
@@ -1,7 +1,13 @@
+<script setup lang="ts">
+import TicTacToeIcon from '@/modules/common/icons/TicTacToeIcon.vue';
+
+</script>
+
 <template>
-  <div class="flex items-center justify-center py-4 px-4 sm:px-6">
-    <p class="mt-2 text-4xl font-semibold tracking-tight text-pretty sm:text-5xl lg:text-balance">
+  <div class="flex flex-col items-center justify-center py-4 px-4 sm:px-6">
+    <p class="mt-4 text-4xl font-semibold tracking-tight text-pretty sm:text-5xl lg:text-balance">
       Tic Tac Toe
     </p>
+    <TicTacToeIcon class="mt-4 w-16 h-16" />
   </div>
 </template>
--- src/modules/home/views/HomeView.vue
+++ src/modules/home/views/HomeView.vue
@@ -1,29 +1,16 @@
-
 <template>
-  <HomeNavbar/>
-  <div class="flex flex-col items-center justify-center w-full py-10">
+  <HomeNavbar />
+  <div class="flex flex-col items-center justify-center w-full py-6">
     <div className="flex flex-col gap-4 items-center w-80">
-      <div class="w-full">
-        <CustomCard>
-          <template #content>
-            <div class="flex flex-col gap-4 text-center">
-              <p class="text-1xl font-bold">Total games played</p>
-              <p class="text-xl font-semibold">0</p>
-            </div>
-          </template>
-        </CustomCard>
-      </div>
-      <RouterLink class="btn w-full py-10 btn-soft btn-primary" :to="{ name: 'players-layout' }">
+      <RouterLink class="btn w-full py-10 btn-soft btn-primary rounded-full" :to="{ name: 'players-layout' }">
         New Game
       </RouterLink>
-      <button class="btn w-full py-8 btn-soft">Options</button>
+      <button class="btn w-full py-8 btn-soft rounded-full">Options</button>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-  import { RouterLink } from 'vue-router';
-  
-  import CustomCard from '@/modules/common/components/CustomCard.vue';
-  import HomeNavbar from '../components/HomeNavbar.vue';
-    
+import { RouterLink } from 'vue-router';
+
+import HomeNavbar from '../components/HomeNavbar.vue';
 </script>
