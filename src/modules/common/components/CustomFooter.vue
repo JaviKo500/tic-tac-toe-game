514e913509597b2583822649e2c6c446ef1672b0
JaViKo500 <javikogutierrez64@gmail.com>
2025-02-19 23:25:45 -0500

    fix(ui): update design footer

--- src/modules/common/components/CustomFooter.vue
+++ src/modules/common/components/CustomFooter.vue
@@ -1,18 +1,53 @@
 <template>
-  <footer class="footer sm:footer-horizontal bg-base-200 text-base-content p-6 flex-none">
-    <aside>
-    <AzuIcon/>
-  </aside>
-  <nav>
-    <h6 class="footer-title">Company</h6>
-    <a class="link link-hover">AzuCompany</a>
-  </nav>
-  <nav>
-    <h6 class="footer-title">Legal</h6>
-    <a class="link link-hover">Terms of use</a>
-  </nav>
-  </footer>
+  <!-- component -->
+  <div
+    class="py-4 rounded-lg mx-auto flex-row items-center justify-between overflow-hidden rounded-lg bg-base-300 px-6 flex w-screen">
+    <div class="flex flex-row items-center space-x-3 text-sm ">
+      <span class="flex items-center">
+        <DateIcon />
+        {{ new Date().toLocaleDateString() }}
+      </span>
+
+      <span class="flex items-center">
+        <a target="_blank" class="hover:text-gray-400" href="https://github.com/JaviKo500" title="Go to github">
+          @javiko500
+        </a>
+      </span>
+    </div>
+
+    <div class="">
+      <ul class="flex items-center space-x-3">
+        <li class="inline-block">
+          <a target="_blank" class="hover:text-[#3b5998]" href="https://www.facebook.com/javier.gutierres.712"
+            title="Go to Facebook">
+            <FacebookIcon />
+          </a>
+        </li>
+        <li class="inline-block">
+          <a target="_blank" class="hover:text-[#00acee]" href="https://twitter.com/javiko500" title="Go to Twitter">
+            <TwitterIcon />
+          </a>
+        </li>
+        <li class="inline-block">
+          <a target="_blank" class="hover:text-red-700" href="https://www.instagram.com/javiko500/"
+            title="Go to Instagram">
+            <InstagramIcon />
+          </a>
+        </li>
+        <li class="inline-block">
+          <a target="_blank" class="hover:text-gray-500" href="https://github.com/JaviKo500" title="Go to github">
+            <GitHubIcon />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
-  import AzuIcon from '../icons/AzuIcon.vue';
+import DateIcon from '../icons/DateIcon.vue';
+import FacebookIcon from '../icons/FacebookIcon.vue';
+import GitHubIcon from '../icons/GitHubIcon.vue';
+import InstagramIcon from '../icons/InstagramIcon.vue';
+import TwitterIcon from '../icons/TwitterIcon.vue';
+
 </script>
