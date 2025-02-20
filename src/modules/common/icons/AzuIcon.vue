e612cf1ae6d8fee532384024a899fbd5475d9c79
JaViKo500 <javikogutierrez64@gmail.com>
2025-02-19 23:02:42 -0500

    add back button

--- src/modules/common/icons/AzuIcon.vue
+++ src/modules/common/icons/AzuIcon.vue
@@ -1,3 +1,10 @@
 <template>
-  <svg width="40" height="40" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 21L7.5 15M18 21L16.5 15M16.5 15L14 5C14 5 13.5 3 12 3C10.5 3 10 5 10 5L7.5 15M16.5 15H7.5" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+  <svg width="40" height="40" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
+    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+      <path d="M6 21L7.5 15M18 21L16.5 15M16.5 15L14 5C14 5 13.5 3 12 3C10.5 3 10 5 10 5L7.5 15M16.5 15H7.5"
+        stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+    </g>
+  </svg>
 </template>
--- src/modules/players/components/CustomMenu.vue
+++ src/modules/players/components/CustomMenu.vue
@@ -1,28 +1,38 @@
-
-
 <template>
   <ul class="menu bg-base-200 rounded-box">
-  <li>
-    <RouterLink :to="{ name: 'home-view' }">
-      <HomeIcon />
-    </RouterLink>
-  </li>
-  <li>
-    <a @click="$emit('open-add-user-modal')">
-      <AddUserIcon />
-    </a>
-  </li>
-  <li>
-    <RouterLink :to="{ name: 'game-layout' }">
-      <InfoIcon />
-    </RouterLink>
-  </li>
-</ul>
+    <li>
+      <RouterLink :to="{ name: 'home-view' }">
+        <HomeIcon />
+      </RouterLink>
+    </li>
+    <li>
+      <a @click="$emit('open-add-user-modal')">
+        <AddUserIcon />
+      </a>
+    </li>
+    <li>
+      <RouterLink :to="{ name: 'game-layout' }">
+        <InfoIcon />
+      </RouterLink>
+    </li>
+    <li>
+      <a @click="back">
+        <BackArrowIcon />
+      </a>
+    </li>
+  </ul>
 </template>
 <script setup lang="ts">
-  import AddUserIcon from '@/modules/common/icons/AddUserIcon.vue';
-  import HomeIcon from '@/modules/common/icons/HomeIcon.vue';
-  import InfoIcon from '@/modules/common/icons/InfoIcon.vue';
+import { useRouter } from 'vue-router';
 
-  defineEmits(['open-add-user-modal']);
+import AddUserIcon from '@/modules/common/icons/AddUserIcon.vue';
+import BackArrowIcon from '@/modules/common/icons/BackArrowIcon.vue';
+import HomeIcon from '@/modules/common/icons/HomeIcon.vue';
+import InfoIcon from '@/modules/common/icons/InfoIcon.vue';
+
+defineEmits(['open-add-user-modal']);
+const router = useRouter();
+const back = () => {
+  router.back();
+}
 </script>
